f68eb879af2ba0a5af68ddfbca1a7e8eda866a46
neutricoindia <166170548+neutricoindia@users.noreply.github.com>
2024-04-20 02:20:05 +0530

    updated with about section

--- src/components/WhoWeAre/WhoWeAre.tsx
+++ src/components/WhoWeAre/WhoWeAre.tsx
@@ -5,130 +5,128 @@ function WhoWeAre() {
   return (
     <div className="wrapper row2">
       <section className="hoc container clear">
-        <div className="sectiontitle">
-          <h2 className="nospace ">Who We Are?</h2>
+        <div className="sectiontitle small">
+          <h2 className="nospace font-lg">Who We Are?</h2>
           <h6 className="heading font-sm">
             We&apos;re a dedicated team leveraging science and technology to
             create tailored, sustainable nutritional solutions for all,
             empowering individuals to thrive.
           </h6>
         </div>
-        <ul id="latest" className="nospace group grid-3">
-          <li className="one_third">
-            <article>
-              <figure>
-                <a className="imgover" href="#">
-                  <Image
-                    src="/images/luis-villasmil-hh3ViD0r0Rc-unsplash.jpg"
-                    alt=""
-                    width={0}
-                    height={0}
-                    sizes="100vw"
-                    style={{
-                      width: "100%",
-                      height: "200px",
-                      objectFit: "cover",
-                    }}
-                  />
-                </a>
-              </figure>
-              <div className="excerpt">
-                <ul className="nospace meta clear">
-                  <li>
-                    <i className="fas fa-user"></i> <a href="#">CEO</a>
-                  </li>
-                  <li>
-                    <time>06 Apr 2045</time>
-                  </li>
-                </ul>
-                <h6 className="heading">Pratip Mazumder</h6>
+      </section>
+      <div
+        className="wrapper bgded"
+        style={{
+          backgroundImage: `url('images/stock-vector-beautiful-pregnant-woman-expecting-a-baby-flat-illustration-2381660481.jpg')`,
+          backgroundPosition: "right",
+          backgroundSize: "contain",
+        }}
+      >
+        <div className="hoc split clear">
+          <section>
+            <p>
+              <strong>NEUTRICO INDIA</strong> is dedicated to the advancement of
+              global health through its research-driven approach to developing
+              nutritional solutions. With a focus on nutraceuticals, the company
+              aims to harness scientific knowledge to create products that
+              promote wellness and prevent various health issues.
+            </p>
+            <p>
+              The company&apos;s commitment to providing formula-driven
+              nutritional products underscores its dedication to addressing both
+              preventative and supplementary measures for health concerns. By
+              translating scientific research into tangible solutions,
+              <strong>NEUTRICO INDIA</strong> strives to uplift the health of
+              individuals worldwide.
+            </p>
+            <p>
+              As a leader in the field of medical nutrition,
+              <strong>NEUTRICO INDIA</strong> highlighting its expertise in
+              developing products that cater to various health needs. Through
+              its innovative approach, the company offers nutritional solutions
+              tailored to specific demographics, in gynecological segment
+            </p>
+            <p>
+              By focusing on areas such as women&apos;s health,
+              <strong>NEUTRICO INDIA</strong> demonstrates its comprehensive
+              approach to addressing diverse health challenges. Through its
+              efforts, the company aims to make a meaningful impact on global
+              health and wellness.
+            </p>
+          </section>
+        </div>
+      </div>
+      <section className="hoc container clear">
+        <section id="services" className="hoc container clear">
+          <ul className="nospace group grid-3">
+            <li className="one_third">
+              <article>
+                <h6 className="heading">
+                  <a href="#">
+                    <i className="fas fa-hand-holding-heart"></i>
+                  </a>
+                  Legacy Of Trust:
+                </h6>
                 <p>
-                  Ipsum fusce a arcu nulla dolor pellentesque imperdiet sagittis
-                  lacinia velit donec eleifend dignissim erat morbi.
+                  NEUTRICO INDIA has established a legacy of trust, acceptance,
+                  and recommendation within the medical fraternity. Through its
+                  commitment to excellence and innovation, the company offers a
+                  diverse range of top-quality products for the healthcare
+                  industry.
                 </p>
-                <footer>
-                  <a href="#">Read More</a>
-                </footer>
-              </div>
-            </article>
-          </li>
-          <li className="one_third">
-            <article>
-              <figure>
-                <a className="imgover" href="#">
-                  <Image
-                    src="/images/dragos-gontariu-GH-mSApoKO0-unsplash.jpg"
-                    alt=""
-                    width={0}
-                    height={0}
-                    sizes="100vw"
-                    style={{
-                      width: "100%",
-                      height: "200px",
-                      objectFit: "cover",
-                    }}
-                  />
-                </a>
-              </figure>
-              <div className="excerpt">
-                <ul className="nospace meta clear">
-                  <li>
-                    <i className="fas fa-user"></i> <a href="#">Admin</a>
-                  </li>
-                  <li>
-                    <time>05 Apr 2045</time>
-                  </li>
-                </ul>
-                <h6 className="heading">Sagittis mauris nulla</h6>
                 <p>
-                  Ultricies pede id eros pellentesque habitant morbi tristique
-                  senectus et netus et malesuada fames ac turpis.
+                  These products are widely utilized and endorsed by healthcare
+                  professionals across the country.
                 </p>
-                <footer>
-                  <a href="#">Read More</a>
-                </footer>
-              </div>
-            </article>
-          </li>
-          <li className="one_third">
-            <article>
-              <figure>
-                <a className="imgover" href="#">
-                  <Image
-                    src="/images/speaker-3.jpg"
-                    alt=""
-                    width={0}
-                    height={0}
-                    sizes="100vw"
-                    style={{
-                      width: "100%",
-                      height: "200px",
-                      objectFit: "cover",
-                    }}
-                  />
-                </a>
-              </figure>
-              <div className="excerpt">
-                <ul className="nospace meta clear">
-                  <li>
-                    <i className="fas fa-user"></i> <a href="#">Admin</a>
-                  </li>
-                  <li>
-                    <time>04 Apr 2045</time>
-                  </li>
-                </ul>
-                <h6 className="heading">Egestas donec augue</h6>
+                <footer>{/* <a href="#">Read More</a> */}</footer>
+              </article>
+            </li>
+            <li className="one_third">
+              <article>
+                <h6 className="heading">
+                  <a href="#">
+                    <i className="fas fa-capsules"></i>
+                  </a>
+                  Research Based methods
+                </h6>
                 <p>
-                  Sed nunc rutrum egestas suspendisse ultrices ipsum non nisi
-                  mauris felis nibh placerat nec feugiat vel vestibulum.
+                  The company&apos;s dedication to evidence-based medicine
+                  translates into products with strong scientific backing,
+                  enhancing their credibility and effectiveness.
                 </p>
-                <footer>
-                  <a href="#">Read More</a>
-                </footer>
-              </div>
-            </article>
-          </li>
-        </ul>
+                <p>
+                  Their commitment to rigorous research ensures the highest
+                  quality standards in their products, providing safe and
+                  effective solutions for healthcare professionals.
+                </p>
+                <footer>{/* <a href="#">Read More</a> */}</footer>
+              </article>
+            </li>
+            <li className="one_third">
+              <article>
+                <h6 className="heading">
+                  <a href="#">
+                    <i className="fas fa-heart"></i>
+                  </a>
+                  Impact & Thrive
+                </h6>
+                <p>
+                  A revolution in personalized wellness: We envision a future
+                  where science-backed nutraceuticals empower individuals to
+                  live their healthiest, happiest lives. (Emphasizes both
+                  personalization and impact)
+                </p>
+                <p>
+                  The global standard for evidence-based wellness: Our vision is
+                  to be the leading provider of innovative, scientifically
+                  proven nutraceutical solutions that elevate global health.
+                  (Focuses on leadership and scientific rigor)
+                </p>
+                <footer>{/* <a href="#">Read More</a> */}</footer>
+              </article>
+            </li>
+          </ul>
+        </section>
       </section>
     </div>
   );
