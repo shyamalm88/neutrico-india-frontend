d9392de50425684d05ff035d1bd7c39cd61f8771
neutricoindia <166170548+neutricoindia@users.noreply.github.com>
2024-09-09 02:56:43 +0530

    updates in meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
     <html lang="en">
       <meta
         name="keywords"
-        content="Neutrico, Neutrico India, NeutricoIndia, Neutrition, nutraceuticals"
+        content="NEUTRICO INDIA, Global health, Nutritional solutions, Nutraceuticals, Scientific research, Wellness products, Preventative health, Supplementary health, Medical nutrition, Nutritional products, Women's health, Gynecological nutrition, Health and wellness, Innovative nutritional solutions, Health supplements, Research-driven nutrition, Nutritional health solutions, Nutraceutical company, Global wellness impact, NEUTRICO INDIA, Global health, Nutritional solutions, Nutraceuticals, Scientific research, Wellness products, Preventative health, Supplementary health, Medical nutrition, Nutritional products, Women's health, Gynecological nutrition, Health and wellness, Innovative nutritional solutions, Health supplements, Research-driven nutrition, Nutritional health solutions, Nutraceutical company, Global wellness impact, Health advancement, Nutrition science, Preventative wellness, Nutritional innovation, Formula-driven health products, Health solutions for women, Scientific nutrition research, Health improvement products, Tailored nutritional supplements, Gynecological health solutions, Dietary supplements for women, Global healthcare solutions, Nutrition for specific demographics, Research-based health supplements, Healthcare nutrition products, Nutraceutical development, Personalized nutrition, Global wellness enhancement, Advanced health solutions, Neutrico, Neutrico India, NeutricoIndia, Neutrition, nutraceuticals"
       />
       <meta property="og:title" content="Neutrico India" />
       <meta
@@ -44,6 +44,7 @@ export default function RootLayout({
 As a leader in the field of medical nutrition, NEUTRICO INDIA highlighting its expertise in developing products that cater to various health needs. Through its innovative approach, the company offers nutritional solutions tailored to specific demographics, in gynecological segment By focusing on areas such as women's health, NEUTRICO INDIA demonstrates its comprehensive approach to addressing diverse health challenges. Through its efforts, the company aims to make a meaningful impact on global health and wellness.`}
       />
       <meta property="og:image" content="/images/logo.png" />
+
       <body id="top">{children}</body>
     </html>
   );
